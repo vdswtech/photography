4e305a4452284f60c65ed77353001166d9223105
Victoria Wolter <daemoneye2@gmail.com>
2023-01-07 07:32:00 -0500

    Add logging features

--- photography.py
+++ photography.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python3
 import argparse
+import os
 import sys
 
 def arguments():
     parser = argparse.ArgumentParser(description='Tool written to manage static website for photography.\nWritten by Victoria Wolter')
 
-    parser.add_argument('-d', '--dir', help="Specify a directory of images to process")
-    parser.add_argument('-i', '--image', help="Specify a image to process")
-    parser.add_argument('-I', '--index', action='store_true', help="Generate index.html")
+    parser.add_argument('-d', '--dir', help="Specify a directory of images to process.")
+    parser.add_argument('-i', '--image', help="Specify a image to process.")
+    parser.add_argument('-I', '--index', action='store_true', help="Generate index.html.")
+    parser.add_argument('-l', '--log', action='store_true', help="Log events to file.")
+    parser.add_argument('-L', '--logfile', help="Specify a logfile to write to. Must set the log flag with this.")
     parser.add_argument('-V', '--version', action='store_true', help="Show version number and exit.")
     parser.add_argument('-v', '--verbose', action='store_true', help="Be verbose.")
 
     return parser.parse_args()
 
-def process(filepath):
+def process(args, filepath):
     pass
 
-def build_index(filepath):
+def build_index(args, filepath):
     pass
+
+def verbose(args, message):
+    if args.verbose:
+        print("[+] " + message)
+
+def log(args, logfile, message):
+    if args.log:
+        logfile.write("[+] " + message)
 
 def main():
     version = '3.1'
@@ -26,6 +37,15 @@ def main():
     if args.version:
         print("Version " + version)
         sys.exit(0)
+
+    if args.logfile == None:
+        log_file = os.path.expanduser('~') + "/.photo.log"
+    else:
+        log_file = logfile
+
+    logfile = open(log_file, "a")
+
+    logfile.close()
 
 if __name__ == "__main__":
     main()
