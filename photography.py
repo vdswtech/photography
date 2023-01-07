9a6c93247a5dd616cbcb8dff0aa7524b33e31820
Victoria Wolter <daemoneye2@gmail.com>
2023-01-07 07:13:21 -0500

    Photography code base code

---
+++ photography.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+def arguments():
+    parser = argparse.ArgumentParser(description='Tool written to manage static website for photography.\nWritten by Victoria Wolter')
+
+    parser.add_argument('-d', '--dir', help="Specify a directory of images to process")
+    parser.add_argument('-i', '--image', help="Specify a image to process")
+    parser.add_argument('-V', '--version', action='store_true', help="Show version number and exit.")
+    parser.add_argument('-v', '--verbose', action='store_true', help="Be verbose.")
+
+    return parser.parse_args()
+
+def process(filepath):
+    pass
+
+def build_index(images):
+    pass
+
+def main():
+    version = '3.1'
+    args = arguments()
+
+    if args.version:
+        print("Version " + version)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
